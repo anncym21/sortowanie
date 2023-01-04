08528c0103c140520f61da76fa6dc043dbc0b70c
budzik12 <81246476+budzik12@users.noreply.github.com>
2023-01-04 08:30:06 +0100

    bubble sort

--- app/src/main/java/com/nkodem/sort/MainActivity.kt
+++ app/src/main/java/com/nkodem/sort/MainActivity.kt
@@ -8,4 +8,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private fun sort(arr:IntArray):IntArray{
+        var swap = true
+        while(swap){
+            swap = false
+            for(i in 0 until arr.size-1){
+                if(arr[i] > arr[i+1]){
+                    val temp = arr[i]
+                    arr[i] = arr[i+1]
+                    arr[i+1] = temp
+                    swap = true
+                }
+            }
+        }
+        return arr
+    }
+    fun main(args:Array<String>){
+        val list = sort(intArrayOf(1,20,3,8,6))
+        for(k in list ) print("$k ")
+    }
 }
